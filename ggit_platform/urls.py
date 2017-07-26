322d7c37ddbf757950289345fcd631cefd071049
AnaStoianov <stoianov_ana@mail.ru>
2017-07-26 12:38:38 +0300

    merge

--- ggit_platform/urls.py
+++ ggit_platform/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url (r'^events$', views.event_list, name='event_list' ),
+    url(r'^regions/$', views.region_list, name='regions_list'),
+    url(r'^members/$', views.member_list, name='members_list'),
 ]
