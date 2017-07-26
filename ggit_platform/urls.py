bf0f123c443c03933ba1278c8e33ce3b0b40b33e
AnaStoianov <stoianov_ana@mail.ru>
2017-07-26 16:21:08 +0300

    d

--- ggit_platform/urls.py
+++ ggit_platform/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^stories/$', views.stories_list, name='stories_list'),
     url(r'^stories/(?P<id>\d+)$', views.stories_detail, name='stories_detail'),
     url(r'^events$', views.event_list, name='event_list'),
+    url(r'^events/(?P<id>\d+)$', views.event_list, name='event_list')
     url(r'^tracks/$', views.track_list, name='track_list'),
     url(r'^tracks/(?P<id>\d+)$', views.track_detail, name='track_detail'),
     url(r'^tracks/$', views.track_list, name='track_list'),
