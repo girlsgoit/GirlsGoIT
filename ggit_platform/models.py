0d99a96012c221bd78b7cb087930d0f59793f50b
SandraRusu17 <sandra.rusu17@gmail.com>
2017-07-26 12:35:48 +0300

    models event

--- ggit_platform/models.py
+++ ggit_platform/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from markdownx.models import MarkdownxField
 
-# Track model
-
-
-# Region model
-
-
-# Member model
-
+class Event(model.Model):
+	title = models.CharField(max_length=50)
+	short_description = models.TextField()
+	start_date = models.DateTimeField()
+	end_date = models.DateTimeField(null =True, blank=True)
+	thumbnail_image = models.CharField(max_length=200)
+	hero_image = models.CharField(max_length=200)
+	aplly_form = models.CharField(max_length=200)
+	long_description = MarkdownxField()
+	application_start_date = models.DateTimeField(null=True, blank=True)
+	application_end_date = models.DateTimeField(null=True, blank=True)
+	region = models.ForeignKey("Region", null=True, blank=True)
+	
+# Create your tests here.
 
 # Event model
 
