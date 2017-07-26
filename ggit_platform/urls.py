9ca18b6a79e55800968dabe3c2b1ce19db82ec8a
Marc Graciov <mark.graciov@gmail.com>
2017-07-26 15:32:24 +0300

    Region list

--- ggit_platform/urls.py
+++ ggit_platform/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^events$', views.event_list, name='event_list'),
     url(r'^tracks/$', views.tracks_list, name='tracks_list'),
     url(r'^track_detail/(?P<id>\d+)$', views.track_detail, name='track_detail'),
-    url(r'^regions/$', views.region_list, name='regions_list'),
+    url(r'^regions/$', views.region_list, name='region_list'),
     url(r'^members/$', views.member_list, name='members_list'),
     url(r'^$', views.index, name='index'),
 ]
