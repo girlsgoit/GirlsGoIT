8cc4d4d32bbc4bf500867cb03aca7c73c8cb8b0a
ElizabetG <elizabet.galaju@gmail.com>
2017-07-26 12:36:41 +0300

    define model track, create list view for track and template for track

--- ggit_platform/urls.py
+++ ggit_platform/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
-    
+    url(r'^tracks/$', views.tracks_list, name='tracks_list'),
+    url(r'^track_detail/(?P<id>\d+)$', views.track_detail, name='track_detail')
 ]
+
--- ggit_platform/views.py
+++ ggit_platform/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Track
 
 # Track views
+def tracks_list(request):
+    tracks = Track.objects.all()
+
+    return render(request, 'track/list.html', {'tracks': tracks})
+
+
+def track_detail(request, id):
+    track = get_object_or_404(Track, id=id)
+
+    return render(request, 'track/detail.html', {'track': track})
 
 
 # Region views
