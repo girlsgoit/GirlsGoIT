fb705659f2fe69e6402c505c71a86396b0b47a57
ElizabetG <elizabet.galaju@gmail.com>
2017-07-27 14:42:04 +0300

    resolve conflict

--- ggit_platform/forms.py
+++ ggit_platform/forms.py
@@ -4,10 +4,8 @@ from .models import Track
 <<<<<<< HEAD
 from .models import Member 
 
-=======
 from .models import Event
 from .models import Story
->>>>>>> 27c0625fb74ac55b1e613fdaa8fe07aafcc276c8
 
 class TrackForm(forms.ModelForm):
 
@@ -16,12 +14,10 @@ class TrackForm(forms.ModelForm):
         fields = '__all__'
 
 
-<<<<<<< HEAD
 class MemberForm(forms.ModelForm):
 
     class Meta:
         model = Member
-=======
 
 class EventForm(forms.ModelForm):
 
@@ -35,5 +31,4 @@ class StoryForm(forms.ModelForm):
 
     class Meta:
         model = Story
->>>>>>> 27c0625fb74ac55b1e613fdaa8fe07aafcc276c8
         fields = '__all__'
