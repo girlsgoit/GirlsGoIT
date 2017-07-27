0c85c60c750b5d5a5b72e539012f6f8230754813
Candrakora <30460056+Candrakora@users.noreply.github.com>
2017-07-27 15:08:27 +0300

    Modificari pentru admin

--- ggit_platform/urls.py
+++ ggit_platform/urls.py
@@ -2,31 +2,31 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^stories/$', views.story_list, name='story_list'),
-    url(r'^stories/new/$', views.story_new, name='story_new'),
-    url(r'^stories/(?P<id>\d+)/edit/$', views.story_edit, name='story_edit'),
-    url(r'^stories/(?P<id>\d+)/delete/$', views.story_delete, name='story_delete'),
+    url(r'^admin/stories/$', views.story_list, name='story_list'),
+    url(r'^admin/stories/new/$', views.story_new, name='story_new'),
+    url(r'^admin/stories/(?P<id>\d+)/edit/$', views.story_edit, name='story_edit'),
+    url(r'^admin/stories/(?P<id>\d+)/delete/$', views.story_delete, name='story_delete'),
 
-    url(r'^events/$', views.event_list, name='event_list'),
-    url(r'^events/new/$', views.event_new, name='event_new'),
-    url(r'^events/(?P<id>\d+)/edit/$', views.event_edit, name='event_edit'),
-    url(r'^events/(?P<id>\d+)/delete/$', views.event_delete, name='event_delete'),
+    url(r'^admin/events/$', views.event_list, name='event_list'),
+    url(r'^admin/events/new/$', views.event_new, name='event_new'),
+    url(r'^admin/events/(?P<id>\d+)/edit/$', views.event_edit, name='event_edit'),
+    url(r'^admin/events/(?P<id>\d+)/delete/$', views.event_delete, name='event_delete'),
 
-    url(r'^tracks/$', views.track_list, name='track_list'),
-    url(r'^tracks/new/$', views.track_new, name='track_new'),
-    url(r'^tracks/(?P<id>\d+)/edit/$', views.track_edit, name='track_edit'),
-    url(r'^tracks/(?P<id>\d+)/delete/$', views.track_delete, name='track_delete'),
+    url(r'^admin/tracks/$', views.track_list, name='track_list'),
+    url(r'^admin/tracks/new/$', views.track_new, name='track_new'),
+    url(r'^admin/tracks/(?P<id>\d+)/edit/$', views.track_edit, name='track_edit'),
+    url(r'^admin/tracks/(?P<id>\d+)/delete/$', views.track_delete, name='track_delete'),
 
-    url(r'^regions/$', views.region_list, name='region_list'),
-    url(r'^regions/(?P<id>\d+)/edit/$', views.region_detail, name='region_detail'),
-    url(r'^regions/new/$', views.region_new, name='region_new'),    
-    url(r'^regions/(?P<id>\d+)/edit/$', views.region_edit, name='region_edit'),
-    url(r'^regions/(?P<id>\d+)/delete/$', views.region_delete, name='region_delete'),
+    url(r'^admin/regions/$', views.region_list, name='region_list'),
+    url(r'^admin/regions/(?P<id>\d+)/edit/$', views.region_detail, name='region_detail'),
+    url(r'^admin/regions/new/$', views.region_new, name='region_new'),    
+    url(r'^admin/regions/(?P<id>\d+)/edit/$', views.region_edit, name='region_edit'),
+    url(r'^admin/regions/(?P<id>\d+)/delete/$', views.region_delete, name='region_delete'),
 
-    url(r'^members/$', views.member_list, name='member_list'),
-    url(r'^members/new/$', views.member_new, name='member_new'),
-    url(r'^members/(?P<id>\d+)/edit/$', views.member_edit, name='member_edit'),
-    url(r'^members/(?P<id>\d+)/delete/$', views.member_delete, name='member_delete'),
+    url(r'^admin/members/$', views.member_list, name='member_list'),
+    url(r'^admin/members/new/$', views.member_new, name='member_new'),
+    url(r'^admin/members/(?P<id>\d+)/edit/$', views.member_edit, name='member_edit'),
+    url(r'^admin/members/(?P<id>\d+)/delete/$', views.member_delete, name='member_delete'),
 
     url(r'^/$', views.index, name='index'),
 ]
--- girlsgoit/urls.py
+++ girlsgoit/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls), 
+    url(r'^django-admin/', admin.site.urls), 
     url(r'^markdownx/', include('markdownx.urls')),
     url(r'^', include('ggit_platform.urls')),
 ]
