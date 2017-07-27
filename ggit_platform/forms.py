eb7ba15f34a9d6016442f193214540982186559a
Candrakora <30460056+Candrakora@users.noreply.github.com>
2017-07-27 13:07:38 +0300

    Import StoryForm in forms

--- ggit_platform/forms.py
+++ ggit_platform/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from .models import Track
 from .models import Event
+from .models import Story
 
 class TrackForm(forms.ModelForm):
 
@@ -15,4 +16,12 @@ class EventForm(forms.ModelForm):
 
     class Meta:
         model = Event
+        fields = '__all__'
+
+        
+
+class StoryForm(forms.ModelForm):
+
+    class Meta:
+        model = Story
         fields = '__all__'
