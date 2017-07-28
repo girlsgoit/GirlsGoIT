a186bdc36ce636b45d8518e17f7880aae651f581
Marc Graciov <mark.graciov@gmail.com>
2017-07-28 12:33:48 +0300

    make some corrections

--- ggit_platform/forms.py
+++ ggit_platform/forms.py
@@ -7,45 +7,38 @@ from .models import Event
 from .models import Story
 from .models import Region
 
-class TrackForm(forms.ModelForm):
-    long_description = MarkdownxFormField()
 
+class TrackForm(forms.ModelForm):
     class Meta:
         model = Track
         fields = '__all__'
 
 
 class MemberForm(forms.ModelForm):
-
     class Meta:
         model = Member
         fields = '__all__'
 
 
 class EventForm(forms.ModelForm):
-    long_description = MarkdownxFormField()
-
     class Meta:
         model = Event
         fields = '__all__'
+
 
 class RegionForm(forms.ModelForm):
     class Meta:
         model = Region
         fields = '__all__'
 
-        
-
 
 class StoryForm(forms.ModelForm):
-    long_description = MarkdownxFormField()
     class Meta:
         model = Story
         fields = '__all__'
 
 
 class RegionForm(forms.ModelForm):
-
     class Meta:
         model = Region
         fields = '__all__'
