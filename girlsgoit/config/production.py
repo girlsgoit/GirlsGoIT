41720a22f66ed290324451807129318a5a357e9b
PlugaruT <plugaru.tudor@gmail.com>
2017-07-28 22:53:22 +0300

    add settings for production

---
+++ girlsgoit/config/production.py
@@ -0,0 +1,21 @@
+import environ
+from .settings import *
+
+env = environ.Env()
+
+INSTALLED_APPS += [
+    'django.contrib.postgres'
+]
+
+# DATABASE CONFIGURATION
+# ------------------------------------------------------------------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('RDS_DB_NAME'),
+        'USER': env('RDS_USERNAME'),
+        'PASSWORD': env('RDS_PASSWORD'),
+        'HOST': env('RDS_HOSTNAME'),
+        'PORT': env('RDS_PORT'),
+    }
+}
