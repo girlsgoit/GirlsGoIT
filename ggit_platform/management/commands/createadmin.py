a2dd39f8d7ebac8af9ceb03be6c85b9e05238b10
Marc Graciov <mark.graciov@gmail.com>
2017-07-29 12:49:21 +0300

    Create superuser command

--- ggit_platform/management/commands/createadmin.py
+++ ggit_platform/management/commands/createadmin.py
@@ -5,4 +5,4 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(username="admin", password="ggitadmin2017")
+            User.objects.create_superuser(username="admin", email='info@tekedu.org', password="ggitadmin2017")
